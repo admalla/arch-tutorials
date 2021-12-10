1e6c7f00dbfb60b5f15e966b469b490b96ab32db
admalla <maasovec@gmail.com>
2021-12-10 11:42:46 +0300

    Создал форму авторизации

---
+++ src/tutorial-2/App.js
@@ -0,0 +1,23 @@
+export default function App() {
+  let email = '';
+  let password = '';
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email = e.target[0].value;
+    password = e.target[1].value;
+    !email || !password ? alert('заполните все поля') : console.log({ email, password });
+    e.target[0].value = '';
+    e.target[1].value = '';
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input style={{ display: 'block', marginBottom: 10 }} type="text" />
+        <input style={{ display: 'block', marginBottom: 10 }} type="password" />
+        <button type="submit">кнопка</button>
+      </form>
+    </div>
+  );
+}
