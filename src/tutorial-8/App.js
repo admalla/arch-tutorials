27a37116e4120aa50636538bb0d15d6d31d6df3b
admalla <maasovec@gmail.com>
2021-12-21 12:01:25 +0300

    Получить список users

---
+++ src/tutorial-8/App.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export default function App() {
+  const [users, setUsers] = useState([]);
+
+  async function handleClickUsers() {
+    let response = await fetch('https://61c080ad33f24c0017823463.mockapi.io/users');
+    let result = await response.json();
+    setUsers(result);
+  }
+
+  return (
+    <div>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.fullName}</li>
+        ))}
+      </ul>
+      <button onClick={handleClickUsers}>Получить список пользователей</button>
+    </div>
+  );
+}
